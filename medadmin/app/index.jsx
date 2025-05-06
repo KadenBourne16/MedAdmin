c677d8eb2d020bb58229bd57893e0be9ac1e4aa2
Kaden16 <78388758+KadenBourne16@users.noreply.github.com>
2025-05-06 11:17:08 +0000

    v1.1
    Added file base routing

---
+++ medadmin/app/index.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View, Button } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Testing</Text>
+      <Button title='Submit'/>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
